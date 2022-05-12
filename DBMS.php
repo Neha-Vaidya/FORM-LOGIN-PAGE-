9940e857f96d5d82381e17bbd0683e1a00d1f7c9
Neha-Vaidya <91827825+Neha-Vaidya@users.noreply.github.com>
2022-05-12 14:37:50 +0530

    Added initial page

---
+++ DBMS.php
@@ -0,0 +1,59 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style1.css"/>
+    <title>Document</title>
+    
+</head>
+<body style="background-image: url('PARK3.jpg'); ">
+  
+    <h1> Personal Info Form </h1>
+    <hr>
+    <form action="DBMS.php" method="post">
+        <label for="fname">Name:</label>
+        <input type="text" id="fname" name="fname"><br><br>
+        <label for="contact">Contact no:</label>
+        <input type="text" id="contact" name="contact"><br><br>
+        <label for="email">Email Id:</label>
+        <input type="text" id="email" name="email"><br><br>
+        <label for="pass">Password:</label>
+        <input type="password" id="pass" name="pass"><br><br>
+        <button class="Submit" name ="Submit">Submit</button>
+      </form>
+      <?php
+if(isset($_POST['Submit'])){
+  $server = 'localhost:3307';
+  $username = 'root';
+  $password = "";
+  $database='form';
+
+  $con= new mysqli ($server,$username,$password);
+
+
+  if(!$con){
+      die("connection to this database failed due to ".mysqli_connect_error);
+
+  }
+else{
+$Name=$_POST['fname'];
+$Contact=$_POST['contact'];
+$Email=$_POST['email'];
+$password=$_POST['pass'];
+ $w ="INSERT INTO `form`.`registration`( `Name`, `Contact`, `EmailId`, `Password`) VALUES ( '".$Name."', '".$Contact."', '".$Email."', '".$password."');";
+ 
+ if(!$w){
+   echo"ERROR: $w <br> $con->error";
+ }
+ $result=$con->query($w);
+}
+
+}
+?>
+</body>
+</html>
+<!--INSERT INTO `registration` (`SrNo`, `Name`, `Contact`, `Email Id`, `Password`, `Time`) VALUES ('1', 'Neha Vaidya', '1111111111', 'neha.vaidya@cumminscollege.in', '12345', current_timestamp());>
+    
